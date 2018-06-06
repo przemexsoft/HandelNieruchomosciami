4c1089bc6e8902185e5a62ddf73441ff32ad0b5e
przemexsoft <przemextwothree@gmail.com>
2018-06-06 21:55:27 +0200

    Poprawa connection stringa w Startup.cs

--- HandelNieruchomosciami/Startup.cs
+++ HandelNieruchomosciami/Startup.cs
@@ -26,7 +26,7 @@ namespace HandelNieruchomosciami
         {
             services.AddMvc();
 
-            var dbConnectionString = @"Server=(localdb)\mssqllocaldb;Database=MistrzowieDB;Trusted_Connection=True;";
+            var dbConnectionString = @"Server=(localdb)\mssqllocaldb;Database=HandelNieruchomosciamiDB;Trusted_Connection=True;";
             services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(dbConnectionString));
         }
 
