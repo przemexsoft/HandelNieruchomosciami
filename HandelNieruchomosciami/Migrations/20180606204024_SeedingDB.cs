12155f41cee258be037ca62179552a0eae623b95
przemexsoft <przemextwothree@gmail.com>
2018-06-06 23:06:16 +0200

    Dodanie seedingu do bazy

---
+++ HandelNieruchomosciami/Migrations/20180606204024_SeedingDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HandelNieruchomosciami.Migrations
+{
+    public partial class SeedingDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Wlasciciels (WlascicielImie, WlascicielNazwisko, WlascicielAdres, WlascicielTelefon, WlascicielEmail) VALUES " +
+                "('Siemas','Ziomal','Adress jakis','657576575','siemas@gmail.com')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete * From Wlasciciels");
+        }
+    }
+}
