e7b63df5f1baf42c22720c472a1e023d8d53500a
przemexsoft <przemextwothree@gmail.com>
2018-06-08 17:40:25 +0200

    Dodanie TransakcjaControllers.cs

---
+++ HandelNieruchomosciami/Controllers/TransakcjaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HandelNieruchomosciami.Controllers.Models.Interfaces;
+
+namespace HandelNieruchomosciami.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Transakcja")]
+    public class TransakcjaController : Controller
+    {
+        //warstwa abstrakcji
+
+        private readonly ITransakcjaRepository _transakcjaRepository;
+
+        public TransakcjaController(ITransakcjaRepository transakcjaRepository)
+        {
+            _transakcjaRepository = transakcjaRepository;
+        }
+
+        [HttpGet("action")]
+        public IActionResult GetTransacties()
+        {
+            return new JsonResult(_transakcjaRepository.GetAll());
+        }
+
+    }
+}
