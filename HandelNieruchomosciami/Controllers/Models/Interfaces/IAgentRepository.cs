4d9f42b00e484d210a0c13bea18c3244506f61f9
majster-gsm <majster-gsm@o2.pl>
2018-06-21 20:46:38 +0200

    Dodanie interfejsu Agenta

---
+++ HandelNieruchomosciami/Controllers/Models/Interfaces/IAgentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandelNieruchomosciami.Controllers.Models.Interfaces
+{
+    public interface IAgentRepository
+    {
+        List<Agent> GetAllAgents();
+        Agent GetAgent(int AgentId);
+
+        int AddAgent(Agent agent);
+        int UpdateAgent(Agent agent);
+        void DeleteAgent(Agent agent);
+    }
+}
