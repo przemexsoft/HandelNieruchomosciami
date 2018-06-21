4c7af339ea6a2770fbc9c599f09ad373d573465d
majster-gsm <majster-gsm@o2.pl>
2018-06-21 20:48:36 +0200

    Dodanie interfejsu Kontrahenta

---
+++ HandelNieruchomosciami/Controllers/Models/Interfaces/IKontrahentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandelNieruchomosciami.Controllers.Models.Interfaces
+{
+    public interface IKontrahentRepository
+    {
+        List<Kontrahent> GetAllKontrahents();
+        Kontrahent GetKontrahent(int KontrahentId);
+
+        int AddKontrahent(Kontrahent kontrahent);
+        int UpdateKontrahent(Kontrahent kontrahent);
+        void DeleteKontrahent(Kontrahent kontrahent);
+    }
+}
